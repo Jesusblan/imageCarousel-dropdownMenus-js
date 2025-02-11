f676eb23b6c7a4c6be5917edad1b35c6cbe1c5f9
JesusBlan <jezublanco@gmail.com>
2025-02-11 14:26:46 +0100

    added a function in js to hide images and display them one by one

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import "./styles.css";
 import { greeting } from "./home.js";
 
-console.log(greeting);
+
+let picIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let pics = document.querySelectorAll(".picFade");
+
+  for (i = 0; i < pics.length; i++) {
+    pics[i].style.display = "none";
+  }
+
+  picIndex++;
+
+  if (picIndex > pics.length) {picIndex = 1}
+  pics[picIndex-1].style.display = "block";
+  setTimeout(showSlides, 2500);
+}
+
+
+
